5c41edaf6a3ddb9c1153be087cb52cad4332a2b6
Andrei-Calin Georgescu <calinandrei8@gmail.com>
2020-06-30 19:46:33 +0300

    Add TaskStorage class

--- TODO-Taskmanager/index.php
+++ TODO-Taskmanager/index.php
@@ -3,8 +3,13 @@
 require 'vendor/autoload.php';
 
 use Todo\Models\Task;
+use Todo\Storage\MySqlDatabaseTaskStorage;
 
-$task = new Task;
-$task->setDescription('Learn oop');
-$task->setDue(new DateTime('+ 2 days'));
-var_dump($task);
+try {
+ 	 $db = new PDO('mysql:host=127.0.0.1;dbname=todo;port=8889', 'root', 'root');
+ } catch (PDOException $e) {
+ 	die('Something went wrong when connecting to the database.');
+ }
+
+$storage = new MySqlDatabaseTaskStorage($db);
+
