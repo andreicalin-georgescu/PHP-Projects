daa7ad986ca3d0caa61c1695a0d0ffd1e41d47d8
Andrei-Calin Georgescu <calinandrei8@gmail.com>
2020-07-03 18:26:59 +0300

    Add basic UI interface to index.php

--- TODO-Taskmanager/index.php
+++ TODO-Taskmanager/index.php
@@ -11,3 +11,42 @@ $dbConnection = DbConfig::getInstance()->getConnection();
 
 $storage = new MySqlDatabaseTaskStorage($dbConnection);
 $manager = new TaskManager($storage);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Task Planner</title>
+	</head>
+	<body>
+		<form action="" method="post">
+			<div class="taskDescr">
+				<label for="taskDescription">Task Description:</label><br>
+				<input type="text" id="taskDescription" name="taskDescription"><br>
+			</div>
+			<div class="taskDue">
+				<label for="taskDue">Task Due Date:</label><br>
+				<input type="datetime-local" id="taskDue" name="taskDue"><br>
+			</div>
+			<div class="button">
+				<input type="submit" name="submitNewTask" value="Submit new task">
+				<input formaction="" method="post" type="submit" name="updateTask" value="Update existing task"><br>
+			</div>
+			
+		</form>
+		<br>
+		<form action="" method="post">
+			<div>
+				<label for="taskId">Task ID:</label><br>
+				<input type="number" id="taskId" name="taskId"><br>
+			</div>
+			<div class="button">
+				<input type="submit" name="submitButton" value="Find task with this ID">
+				<input type="submit" name="submitButton" value="Delete task with this ID">
+			</div>
+		</form>
+		<br>
+		<button class="button" name="getAll" value="getAll">Get all tasks</button><br>
+	</body>
+</html>
