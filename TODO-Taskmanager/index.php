0adf76be47a90a3db8fe9e34930da4f27cde28a7
Andrei-Calin Georgescu <calinandrei8@gmail.com>
2020-07-10 19:10:52 +0300

    Adds separate file for custom styling. Changes various UI elems in index.

--- TODO-Taskmanager/index.php
+++ TODO-Taskmanager/index.php
@@ -14,75 +14,52 @@ $manager = TaskManager::getInstance();
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
 	<title>Task Planner</title>
 	</head>
 	<body>
-		<style>
-
-			.jumbotron {
-				background: #66ccff;
-				color: white;
-			}
-
-			/* Submit jumbo */
-
-			#jumbo1 {
-				min-width: 338px;
-				right: 2rem;
-
-			}
-
-			/* Get Task jumbo */
-
-			#jumbo2 {
-				min-width: 338px;
-				right: 2rem;
-				top: 0px;
-
-			}
-			
-			/* Show all current tasks jumbo */
-			#jumbo3 {
-				min-width: 338px;
-			}
-			
-		</style>
-		<div class="container" id="pag-header">
-			<div class="page-header">
+		<div class="d-flex p-2" id="page-header">
+			<div class="pb-2 mt-4 mb-2 bd-highlight">
 				<h1>Task Planner</h1>
 			</div>
 		</div>
 		<br>
+
 		<div class="container">
 			<div class="row">
-				<div class="col-md-6 dflex flex-md-column">
+				<div class="col-md-8">
 					<?php
-						if(isset($_POST['getAll'])) {
-							$tasks = $manager->getAllTasks();
-							echo '<div class="jumbotron" id="jumbo3">
-										<h2>Current Tasks</h2>';
-										foreach ($tasks as $task) {
-											echo '<table>
-												<tr>
-													<th> ID </th>
-													<th> Description </th>
-													<th> Due Date </th>
-													<th> Status </th>
-												</tr>';
+					if(isset($_POST['getAll'])) {
+						$tasks = $manager->getAllTasks();
+						echo '<div class="jumbotron" id="jumbo3">
+								<table class="table table-bordered table-striped table-hover">
+									<thead>
+										<tr>
+											<td colspan="4"> <h2>Current Tasks</h2></td>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
 
-										echo '<tr><td>' , $task->getId() , '</td>';
-										echo '<td>' , $task->getDescription() , '</td>'; 
-										echo '<td>' , $task->getDue()->diffForHumans() , '</td>'; 
-										echo '<td>' , ($task->getComplete()) ? 'Complete' : 'Due' , '</td></tr></table>'; 	
-										}
+											<th class="text-center"> ID </th>
+											<th class="text-center"> Description </th>
+											<th class="text-center"> Due Date </th>
+											<th class="text-center"> Status </th>
+										</tr>';
 
-							echo '</div>';
+						foreach ($tasks as $task) {
+							echo '<tr><td>' , $task->getId() , '</td>';
+							echo '<td>' , $task->getDescription() , '</td>';
+							echo '<td>' , $task->getDue()->diffForHumans() , '</td>';
+							echo '<td>' , ($task->getComplete()) ? 'Complete' : 'Due' , '</td></tr>';
 						}
-					?>
+						echo '</tbody></table></div>';
+					}
+				?>
 				</div>
-
-				<div class="col-md-6">
-					<div class="float-md-right">
+				<div class="col-md-4">
+					<div class="col-md-6 mw-100">
 						<div class="jumbotron" id="jumbo1">
 							<h3>Submit a new task</h3>
 
@@ -95,34 +72,34 @@ $manager = TaskManager::getInstance();
 								<input class="btn btn-primary" type="submit" name="submitNewTask" value="Submit new task">
 							</form>
 						</div>
-					</div>
 				</div>
-			</div>
-		</div>
-		
-		<div class="container">
-			<div class="row justify-content-end">
-				<div class="col-md-4">
+
+				<div class="col-md-6 mw-100">
 					<div class="jumbotron" id="jumbo2">
-						<h3>Get details about a task</h3>
-						<form action="/app/Interact/GetTask.php" method="post" autocomplete="off">
-							<label for="taskId">Task ID:</label><br>
-							<input type="number" id="taskId" name="taskId"><br>
-							<br>
-							<input class="btn btn-primary" type="submit" name="submitButton" value="Find task with this ID"><br>
-							<br>
-							<h3> Or </h3>
-							<input class="btn btn-primary" type="submit" formaction="/index.php" name="getAll" value="Get All Tasks">
-						</form>
+							<h3>Get details about a task</h3>
+							<form action="/app/Interact/GetTask.php" method="post" autocomplete="off">
+								<label for="taskId">Task ID:</label><br>
+								<input type="number" id="taskId" name="taskId"><br>
+								<br>
+								<input class="btn btn-primary" type="submit" name="submitButton" value="Find task with this ID"><br>
+								<br>
+								<h3> Or </h3>
+								<input class="btn btn-primary" type="submit" formaction="/index.php" name="getAll" value="Get All Tasks">
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+		<footer class="page-footer font-small blue">
+			<div class="footer-copyright text-left py-3">© 2020 Andrei-Calin Georgescu</div>
+		</footer>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 	</body>
 </html>
